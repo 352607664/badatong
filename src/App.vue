e58ff02b10af8294d67cef7a2f7796760feef30b
352607664 <352607664@qq.com>
2026-08-16 22:23:47 +0800

    upload src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,36 @@
+<script setup>
+import NavBar from './components/NavBar.vue'
+import HeroSection from './components/HeroSection.vue'
+import ServicesSection from './components/ServicesSection.vue'
+import PlansSection from './components/PlansSection.vue'
+import WhyUsSection from './components/WhyUsSection.vue'
+import ProcessSection from './components/ProcessSection.vue'
+import AboutSection from './components/AboutSection.vue'
+import FaqSection from './components/FaqSection.vue'
+import ContactSection from './components/ContactSection.vue'
+import FooterSection from './components/FooterSection.vue'
+</script>
+
+<template>
+  <div class="app-root">
+    <NavBar />
+    <main>
+      <HeroSection />
+      <ServicesSection />
+      <PlansSection />
+      <WhyUsSection />
+      <ProcessSection />
+      <AboutSection />
+      <FaqSection />
+      <ContactSection />
+    </main>
+    <FooterSection />
+  </div>
+</template>
+
+<style scoped>
+.app-root {
+  min-height: 100vh;
+  overflow-x: hidden;
+}
+</style>
